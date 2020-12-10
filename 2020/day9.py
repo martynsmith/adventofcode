355735112951f736c8fac33f0a64a03a95d3c62d
Martyn Smith <martyn@dollyfish.net.nz>
2020-12-10 23:13:08 +1300

    day9

---
+++ 2020/day9.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import itertools
+
+data = open('day9.txt').readlines()
+
+preamble_length = 25
+
+buf = []
+
+for line in data:
+    n = int(line.strip())
+    if len(buf) < preamble_length:
+        buf.append(n)
+        continue
+
+    if not any(
+            (x, y) for x, y in itertools.combinations(buf, 2) if x + y == n and x != y
+    ):
+        part1 = n
+        break
+
+    buf.append(n)
+    buf.pop(0)
+
+print(f"part1: {part1}")
+
+buf = []
+for line in data:
+    n = int(line.strip())
+    buf.append(n)
+
+    while sum(buf) > part1:
+        buf.pop(0)
+
+    if len(buf) >= 2 and sum(buf) == part1:
+        break
+
+print(f"part2: {min(buf) + max(buf)}")
