9f8cba4775fe1504b94d0f434f2b4975828630e9
Martyn Smith <martyn@dollyfish.net.nz>
2020-12-14 11:27:43 +1300

    Refactored and removed debugging crap

--- 2020/day13.py
+++ 2020/day13.py
@@ -16,49 +16,20 @@ wait_minutes, next_bus = sorted(schedule)[0]
 
 print(f"part1: {wait_minutes * next_bus}")
 
-checks = []
 timestamp, timestamp_delta = busses.pop(0)
-
-
-def is_valid():
-    for offset, bus in checks:
-        if (timestamp + offset) % bus != 0:
-            return False
-    return True
-
 
 while busses:
     offset, bus = busses.pop(0)
-    checks.append((offset, bus))
 
-    while not is_valid():
+    while (timestamp + offset) % bus:
         timestamp += timestamp_delta
 
     ts1 = timestamp
     timestamp += timestamp_delta
-    while not is_valid():
+    while (timestamp + offset) % bus:
         timestamp += timestamp_delta
 
     timestamp_delta = timestamp - ts1
     timestamp = ts1
 
-
-print("part2: 775230782877242")
 print(f"part2: {timestamp}")
-
-#
-# print(timestamp)
-
-# from sympy import *
-#
-# a, b, t = symbols('a b t', integer=True)
-# init_printing(use_unicode=True)
-
-# print(
-#     solve(
-#         Eq(23 * a - t, 41 * b - 13 - t),
-#         t, a, b,
-#         dict=True,
-#         set=True,
-#     )
-# )
